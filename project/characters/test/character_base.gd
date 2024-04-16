96ada5d5d6d48df0003cf6ac23016a5defda5861
Dmitry Sokolov <Hellbarsik@yandex.ru>
2024-04-16 15:20:41 +0300

    Add looking at target

--- project/characters/test/character_base.gd
+++ project/characters/test/character_base.gd
@@ -2,14 +2,20 @@ extends CharacterBody3D
 
 @export var speed : float = 2.0
 @export var jump_velocity : float = 4.0
+@export var fight_target_node : NodePath
 
 @onready var animation_player : AnimationPlayer = $Mesh/AnimationPlayer
+
+var fight_target : Node = null
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var movement_direction : Vector2 = Vector2.ZERO
 
 func _ready():
 	animation_player.add_animation_library("hero_animations", load("res://characters/hero/animations/hero_animations.tres"))
+	if not fight_target_node.is_empty():
+		fight_target = get_node(fight_target_node)
+	
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -20,10 +26,17 @@ func _physics_process(delta):
 		velocity.z = movement_direction.y * speed
 		animation_player.play("hero_animations/walk_1")
 		
-		var look_target : Vector3 = global_position - velocity.normalized()
+		var look_target : Vector3 = Vector3.ZERO
+		
+		if fight_target == null:
+			look_target = global_position - velocity.normalized()
+		else:
+			look_target = fight_target.global_position
+		
 		look_target.y = global_position.y
+		
 		if look_target.length() > 0:
-			look_at(look_target, Vector3.UP)
+			look_at(look_target, Vector3.UP, true)
 	else:
 		velocity.x = 0
 		velocity.z = 0
